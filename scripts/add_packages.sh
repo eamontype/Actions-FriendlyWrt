3b23385bb7bb4c935f999a8b6e47b7c39872ee7b
eamon <eamonxiong@gmail.com>
2024-12-07 03:52:55 +0800

    优化脚本

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -1,55 +1,90 @@
 #!/bin/bash
 
+# 日志输出
+log() {
+    echo "[INFO] \\$1"
+}
+
+# 下载并保存文件到指定路径
+download_file() {
+    local url=\$1
+    local dest=\$2
+    log "Downloading $url to $dest"
+    if ! wget -q "$url" -O "$dest"; then
+        echo "[ERROR] Failed to download $url" >&2
+        exit 1
+    fi
+}
+
+# 克隆 git 仓库到指定目录
+clone_repo() {
+    local repo_url=\$1
+    local dest_dir=\$2
+    local branch=${3:-master}
+    log "Cloning $repo_url into $dest_dir"
+    if [ -d "$dest_dir" ]; then
+        rm -rf "$dest_dir"
+    fi
+    if ! git clone --depth 1 -b "$branch" "$repo_url" "$dest_dir"; then
+        echo "[ERROR] Failed to clone $repo_url" >&2
+        exit 1
+    fi
+}
+
+# 添加配置到指定文件
+add_config() {
+    local config_content=\$1
+    config_contents="$config_contents$config_content"
+}
+
+# 初始化配置内容变量
+config_contents=""
+
+# {{ Add common utils
+add_config "
+CONFIG_PACKAGE_nano=y
+CONFIG_PACKAGE_lm-sensors=y
+"
+
 # {{ Add luci-app-diskman
 (cd friendlywrt && {
     mkdir -p package/luci-app-diskman
-    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+    download_file https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile package/luci-app-diskman/Makefile
     mkdir -p package/parted
-    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+    download_file https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile package/parted/Makefile
 })
-cat >> configs/rockchip/01-nanopi <<EOL
+
+add_config "
 CONFIG_PACKAGE_luci-app-diskman=y
 CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs=y
 CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
 CONFIG_PACKAGE_smartmontools=y
-EOL
-# }}
+"
 
 # {{ Add luci-app-openclash
-(cd friendlywrt && {
-    # 克隆 OpenClash 的源代码
-    [ -d package/luci-app-openclash ] && rm -rf package/luci-app-openclash
-    git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash --depth 1 -b master
-})
+(cd friendlywrt && clone_repo https://github.com/vernesong/OpenClash.git package/luci-app-openclash)
 
-# 修改 OpenWrt 配置文件，启用 OpenClash 插件
-cat >> configs/rockchip/01-nanopi <<EOL
+add_config "
 # 启用 luci-app-openclash
 CONFIG_PACKAGE_luci-app-openclash=y
-CONFIG_PACKAGE_lm-sensors=y
-EOL
-# }}
+"
 
 # {{ Add luci-app-vlmcsd
-(cd friendlywrt && {
-    [ -d package/luci-app-vlmcsd ] && rm -rf package/luci-app-vlmcsd
-    git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd --depth 1 -b master
-})
+(cd friendlywrt && clone_repo https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd)
 
-cat >> configs/rockchip/01-nanopi <<EOL
+add_config "
 # 启用 luci-app-vlmcsd
 CONFIG_PACKAGE_luci-app-vlmcsd=y
 CONFIG_PACKAGE_luci-i18n-vlmcsd-zh-cn=y
-EOL
-# }}
+"
 
 # {{ Add luci-theme-argon
-(cd friendlywrt/package && {
-    [ -d luci-theme-argon ] && rm -rf luci-theme-argon
-    git clone https://github.com/jerrykuku/luci-theme-argon.git --depth 1 -b master
-})
-echo "CONFIG_PACKAGE_luci-theme-argon=y" >> configs/rockchip/01-nanopi
+(cd friendlywrt/package && clone_repo https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon)
+
+add_config "CONFIG_PACKAGE_luci-theme-argon=y"
+
 sed -i -e 's/function init_theme/function old_init_theme/g' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
+
 cat > /tmp/appendtext.txt <<EOL
 function init_theme() {
     if uci get luci.themes.Argon >/dev/null 2>&1; then
@@ -59,4 +94,8 @@ function init_theme() {
 }
 EOL
 sed -i -e '/boardname=/r /tmp/appendtext.txt' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
-# }}
+
+# 将所有配置内容一次性写入目标文件
+echo "$config_contents" >> configs/rockchip/01-nanopi
+
+log "Script completed successfully."
