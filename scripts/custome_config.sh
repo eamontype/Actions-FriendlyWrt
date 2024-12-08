78338d0192b5a0282c9f9a7bdb1af16b0ee9cc1d
eamon <eamonxiong@gmail.com>
2024-12-09 00:27:58 +0800

    修复转义bug

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -11,17 +11,17 @@ cat <<EOF > ./friendlywrt/files/etc/uci-defaults/99-custom
 #!/bin/sh
 # 根据网卡数量配置网络
 count=0
-for iface in $(ls /sys/class/net | grep -v lo); do
+for iface in \$(ls /sys/class/net | grep -v lo); do
   # 检查是否有对应的设备，并且排除无线网卡
-  if [ -e /sys/class/net/$iface/device ] && [[ $iface == eth* || $iface == en* ]]; then
-    count=$((count + 1))
+  if [ -e /sys/class/net/\$iface/device ] && [[ \$iface == eth* || \$iface == en* ]]; then
+    count=\$((count + 1))
   fi
 done
-if [ "$count" -eq 1 ]; then
+if [ "\$count" -eq 1 ]; then
     # 单个网卡，设置为 DHCP 模式
     uci set network.lan.proto='dhcp'
     uci commit network
-elif [ "$count" -gt 1 ]; then
+elif [ "\$count" -gt 1 ]; then
     # 多个网卡，保持静态 IP
     uci set network.lan.ipaddr='192.168.74.1'
     uci commit network
